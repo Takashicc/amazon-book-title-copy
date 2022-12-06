1c24597ffd1414410b9e8e9cacafd046f8251f47
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-12-06 19:07:33 +0900

    style: Format indent space

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,29 +2,29 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    background: path.resolve(__dirname, "../src/background.ts"),
-  },
-  output: {
-    path: path.join(__dirname, "../dist"),
-    filename: "[name].js",
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/,
-      },
+    mode: "production",
+    entry: {
+        background: path.resolve(__dirname, "../src/background.ts"),
+    },
+    output: {
+        path: path.join(__dirname, "../dist"),
+        filename: "[name].js",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: ".", to: ".", context: "public" }],
+        }),
     ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: ".", to: ".", context: "public" }],
-    }),
-  ],
 };
